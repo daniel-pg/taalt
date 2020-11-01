092b37289b55c90bf5db7616c0e943d8ae4e5249
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-11-01 19:19:59 -0400

    Cria setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='taalt',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Daniel Paulo Garcia',
+    author_email='',
+    description=''
+)
